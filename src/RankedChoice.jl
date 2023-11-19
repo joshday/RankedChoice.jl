75ac33221d96023b1f00c61297b9e60eac8764f0
Josh Day <emailjoshday@gmail.com>
2023-11-19 07:23:08 -0500

    first commit

--- src/RankedChoice.jl
+++ src/RankedChoice.jl
@@ -1,5 +1,45 @@
 module RankedChoice
 
-# Write your package code here.
+export Poll, vote!, votes, rounds
+
+#-----------------------------------------------------------------------------# Poll
+struct Poll
+    prompt::String
+    candidates::Vector{String}
+    ballots::Vector{Vector{Int}}
+end
+Poll(prompt::String, candidates::Vector{String}) = Poll(prompt, candidates, Vector{Int}[])
+
+vote!(p::Poll, v::AbstractVector{Int}) = (push!(p.ballots, v); p)
+
+function vote!(p::Poll, v::AbstractVector{String})
+    @assert all(c -> c in p.candidates, v)
+    vote!(p, findfirst.(isequal.(v), p.candidates))
+end
+
+function votes(p::Poll, set = Set(1:length(p.candidates)))
+    map(p.ballots) do x
+        i = findfirst(j -> j in set, x)
+        isnothing(i) ? nothing : x[i]
+    end
+end
+
+function rounds(p::Poll)
+    n = length(p.candidates)
+    set = Set(1:n)
+    out = Vector{Int}[]
+    for _ in 1:n
+        vts = votes(p, set)
+        counts = [sum(vts .== i) for i in 1:n]
+        push!(out, counts)
+        any(counts .> length(vts) / 2) && break
+        _mincount = minimum(counts[i] for i in set)
+        for loser in findall(counts .== _mincount)
+            delete!(set, loser)
+        end
+    end
+    return out
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using RankedChoice
 using Test
+using Random
 
 @testset "RankedChoice.jl" begin
-    # Write your tests here.
+    @testset "Simple" begin
+        poll = Poll("Favorite Color", ["Red", "Green", "Blue", "Yellow", "Orange", "Purple"])
+        for i in 1:10^6
+            vote!(poll, randperm(6))
+        end
+    end
 end
